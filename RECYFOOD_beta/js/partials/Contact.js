c5a5ee099a29681ed5410457e9cd8d9ff5eca33e
kodola <a.kodzis13@gmail.com>
2020-04-03 14:29:54 +0200

    contact infos

--- RECYFOOD_beta/js/partials/Contact.js
+++ RECYFOOD_beta/js/partials/Contact.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <h3>Contact</h3>
+        <>
+            <section>
+                <div className="banner nav_content">
+                    <div className="nav_background"/>
+                    <div className="nav_elements">
+                        <div className="nav_header">KONTAKT</div>
+                        <div className="nav_description">
+                                <p>author: kodola</p>
+                                <p>e-mail: soon ;)</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <div className="footer"> </div>
+            </footer>
+        </>
     )
 };
 export default Contact;
