4a32fef08b1255743fe7c926e90d81dc315ec590
kodola <a.kodzis13@gmail.com>
2020-04-18 23:02:36 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     devServer: {
         contentBase: path.join(__dirname, `${entryPath}`),
-        publicPath: "/build/",
+        //publicPath: "/build/",
         compress: true,
         port: 3001
     },
